3c539c7ace4463671b38b6547c47d549fa8f212f
Pushkar Gupta <pushkar_scaler@Pushkars-Office-Air.local>
2026-06-09 13:32:50 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         Scanner scanner = new Scanner(System.in);
+        Set<String> builtins = Set.of("echo", "exit", "type");
+
         while (true) {
             System.out.print("$ ");
             String input = scanner.nextLine();
-            if (input.equals("exit")) {
+
+            if (input.equals("exit") || input.equals("exit 0")) {
                 break;
-            }else if (input.startsWith("echo ")) {
+            } else if (input.startsWith("type ")) {
+                String command = input.substring(5).trim();
+                if (builtins.contains(command)) {
+                    System.out.println(command + " is a shell builtin");
+                } else {
+                    System.out.println(command + ": not found");
+                }
+            } else if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
             } else {
                 System.out.println(input + ": command not found");
