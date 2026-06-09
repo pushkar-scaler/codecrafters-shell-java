8fc7076e6fa209eab3477310dfe03a6bd624fdc1
Pushkar Gupta <pushkar_scaler@Pushkars-Office-Air.local>
2026-06-09 13:30:58 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,11 @@ public class Main {
             String input = scanner.nextLine();
             if (input.equals("exit")) {
                 break;
+            }else if (input.startsWith("echo ")) {
+                System.out.println(input.substring(5));
+            } else {
+                System.out.println(input + ": command not found");
             }
-            System.out.println(input + ": command not found");
         }
     }
 }
